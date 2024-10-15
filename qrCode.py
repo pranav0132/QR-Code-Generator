80d8c9f6b862c9c35912c03fae9a2f8fd4a9623f
Pranav Sharma <93445262+pranav0132@users.noreply.github.com>
2024-10-15 16:31:04 +0530

    Update qrCode.py

--- qrCode.py
+++ qrCode.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 
 #Creating the window
 wn = Tk()
-wn.title('DataFlair QR Code Generator')
+wn.title('Pranav QR Code Generator')
 wn.geometry('700x700')
 wn.config(bg='SteelBlue3')
 
@@ -21,12 +21,12 @@ def generateCode():
     fileDirec=loc.get()+'\\'+name.get() #Getting the directory where the file has to be save
     img.save(f'{fileDirec}.png') #Saving the QR Code
     #Showing the pop up message on saving the file
-    messagebox.showinfo("DataFlair QR Code Generator","QR Code is saved successfully!")
+    messagebox.showinfo("Pranav QR Code Generator","QR Code is saved successfully!")
     
 #Label for the window
 headingFrame = Frame(wn,bg="azure",bd=5)
 headingFrame.place(relx=0.15,rely=0.05,relwidth=0.7,relheight=0.1)
-headingLabel = Label(headingFrame, text="Generate QR Code with DataFlair", bg='azure', font=('Times',20,'bold'))
+headingLabel = Label(headingFrame, text="Generate QR Code with Pranav", bg='azure', font=('Times',20,'bold'))
 headingLabel.place(relx=0,rely=0, relwidth=1, relheight=1)
 
 #Taking the input of the text or URL to get QR code 
